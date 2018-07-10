078476d1c1309ad257aa255a6d6379df3b43a499
Belal555 <40669543+Belal555@users.noreply.github.com>
2018-07-10 15:29:06 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -1,5 +1,5 @@
-# Name:
-# Date:
+# Name: Hawi, Belal, Ryan
+# Date: 07/10/18
 
 
 """ 
@@ -11,5 +11,26 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+import random
+counter = 0
+play_again = 1
 
-
+while int(play_again) != 0:
+    if int(play_again) == 1:
+        random_number = random.randint(1, 9)
+        print ("I'm thinking of a number between 1 and 9. Can you guess my number?")
+        for counter in range (1, 4):
+            Guess = raw_input("Enter a number, or '0' to end the game: ")
+            if int(Guess) == 0:
+                print("Wimp! Why are you at VSA?")
+                break
+            elif int(Guess) < random_number:
+                print "Your number is too low!"
+            elif int(Guess) > random_number:
+                print "Your number is too high!"
+            elif int(Guess) == random_number:
+                print "Congratulations, you guessed my number! You used ",counter, " guesses"
+                break
+            if counter == 3:
+                print("You ran out of guesses! Idiot! I thought smart kids went to this camp.")
+    play_again = raw_input("Would you like to play again? 1 for yes, 0 for no. ")
